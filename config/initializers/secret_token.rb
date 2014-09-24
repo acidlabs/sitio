0521a2dd83897ab3acaa9f2baa3e933e1a9ba88d
patricio martinez <patricio.martinez@bicevida.cl>
2014-09-24 12:44:58 -0300

    Add generated Rails App generated by: rails _4.0.8_ new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sitio::Application.config.secret_key_base = '0fd5f2be5c3dfe9b9c41be9fde32e60cc7f2d58fdff45cc4a2091677e4bfc883d333a3cda88d568df4022e8afcc52535e403b9c015ed703673ce098a309c3439'
