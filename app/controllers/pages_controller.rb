588e7154b4840ae7d4ce60e0da5601e1f71d1442
Gabriel Hourton Balsells <gabriel.hourton@bicevida.cl>
2014-09-24 14:20:42 -0300

    Se agrega primer ejemplo de ruby

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+	def index
+
+		# Escribo en el log (server).
+		puts 'Esto ocurrió en el controlador PagesController'
+		puts 'params >>> ' + params.inspect
+		
+		# Para mostrarlo en la vista
+		@nombre = params[:nombre]
+
+		render 'pages/custom.html',
+		status: :ok,
+		layout: 'bice_vida'
+	end
+end
