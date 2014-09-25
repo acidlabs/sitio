1686f494a94f11c189cece8796c1163f99bc8674
Felipe Aguilera Tapia <felipe.aguilera@gmail.com>
2014-09-25 13:13:53 -0300

    Se modifica la API, mueve '/about' a '/nosotros'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+#retorna algunas instrucciones o las gracias
+def display_instructions_or_tranks
+
+ if @name == 'Invitado'
+  'Puedes decirme tu nombre agregando <b>?=nombre=Manuel</b> a la URL.'.html_safe
+ else
+  "Gracias #{@name} por decirme tu nombre!"
+ end
+
+end
+
+
 end
