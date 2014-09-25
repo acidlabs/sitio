ad37d5dccf6c6a59cc3da5d680ce249e6b5b0b11
Romina Silva <romina.silva@bicevida.cl>
2014-09-25 10:41:27 -0300

    Se agregan archivos

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def bice
+    @name= params[:nombre]||'Invitado'
+    render 'pages/custom' , 
+           status: :ok ,
+           layout: 'bice_vida'
+  end
+  def about
+  end
+end
