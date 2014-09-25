e2bf5149f787d8c3696ee1cbf214e85a2754ebff
Gabriel Hourton Balsells <gabriel.hourton@bicevida.cl>
2014-09-25 13:16:45 -0300

    Se agrega route helper, se agrega ruby en string

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def display_instructions_or_thanks
+		if @name == 'Invitado'
+			'Puedes decirme tu nombre agregando <b> ?nombre=Manuel</b> en la URL'.html_safe
+		else
+			"Gracias #{@name} por decirme tu nombre."
+		end
+	end
 end
