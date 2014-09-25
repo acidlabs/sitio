d38093972c9e06ed74da8419841899cafc37ae3e
Alejandro Carmona <alejandro.carmona@bicevida.cl>
2014-09-25 13:15:15 -0300

    Add route helper, interpolate some ruby strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def display_instructions_or_thanks   
+	    if @name == 'Invitado'
+	      'Puedes decirme tu nombre agregando <b>?=nombre=Manuel</b> a la URL.'.html_safe
+	     else
+	      "Gracias #{@name} por decirme tu nombre!"
+		 end 
+	end
 end
