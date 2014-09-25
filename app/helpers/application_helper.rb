43e1498da69063581d0f40a1f5de0bfac27dfe40
Jorge Cademartori <jorge.cademartori@bicevida.cl>
2014-09-25 13:14:16 -0300

    Modify app API, move '/about' to '/nosotros'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	#devuelve alguna indicaciones de ayuda al usuario de la pagina
+	def display_instructions_or_thanks
+		if @name == 'Invitado'
+    		'Puedes decirme tu nombre agregando <b>?=nombre=Manuel</b> a la URL.'
+  		else
+    		"<strong>Gracias #{@name} por decirme tu nombre!</strong>".html_safe
+    	end
+	end
 end
