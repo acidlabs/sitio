30259c70a7dbc56c06e40c31231d1a1d4d15bf5d
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-09-25 13:15:19 -0300

    Add route helper, interpolate some Ruby in stings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  # Retruns some instructions or a thankful message
+  def display_instructions_or_thanks
+    if @name == 'Invitado'
+      'Puedes decirme tu nombre agregando <b>?nombre=Manuel</b> a la URL.'.html_safe
+    else
+      "Gracias #{@name} por decirme tu nombre!"
+    end
+  end
 end
