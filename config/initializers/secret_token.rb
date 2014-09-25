71169b2616f989c1b2f23d415540964901e28e46
Felipe Hermosilla <felipe.hermosilla@bicevida.cl>
2014-09-25 10:34:46 -0300

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sitio::Application.config.secret_key_base = '4dc541840c1fe76f19249153994306946c852c549af406fddb3572f09cf41401150338a3d1dbfee243581c3221353e2f44244e4b3e38677aac0c8f32e6c9462d'
