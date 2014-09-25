8b297f98e6444e390f96d6a836f3c45665985edd
Joaquin Cea medel <joaquin.cea@bicevida.cl>
2014-09-25 13:48:51 -0300

    Se agrega helper y asset

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Retruns some instructions or a thankful message
+  def display_instructions_or_thanks
+    if @name == 'Invitado'
+      'Puedes decirme tu nombre agregando <b>?nombre=Manuel</b> a la URL.'.html_safe
+    else
+      "<span style='color: red' onblur='alert('hola')'><strong>Gracias #{@name} por decirme tu nombre!</span></strong>".html_safe
+    end
+  end
 end
