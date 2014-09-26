874e1fd7a11d5477243be9fe9b0aceac796c0d85
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-09-26 10:35:38 -0300

    Refactor routes to make modification easier

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Sitio::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#bice'
+  get '/' => 'pages#bice', as: :root
 
   get '/about' => 'pages#about', as: :nosotros
 
